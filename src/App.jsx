ecea07f0770e94a3892a7eb7a59ad9f7645192bd
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2023-07-09 22:46:29 +0530

    feat

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import "./App.css";
 import Continents from "./pages/Continents";
 import Destination from "./pages/Destination";
 import Country from "./pages/Country";
+import Details from "./pages/Details";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Continents />} />
-        <Route path="/countries/:countryId" element={<Country />} />
-        <Route path="/destinations/:destinationId" element={<Destination />} />
+        <Route path="/continents/:continentId" element={<Country />} />
+        <Route path="/countries/:countryId" element={<Destination />} />
+        <Route path="/destinations/:destinationId" element={<Details />} />
       </Routes>
     </div>
   );
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,4 +1,22 @@
+import { useParams } from "react-router";
+import Card from "../components/card/Card";
 import "./styles.css";
+import { data } from "../db";
 export default function Country() {
-  return <div>Country</div>;
+  const { id } = useParams();
+  console.log(id);
+  const findCountry = data.continents(({ id: ID }) => ID == id);
+  console.log(findCountry);
+  return (
+    <div className="page-layout">
+      <h1>Welcome to Trip Advisor</h1>
+      <h2>Top Continents for your next holiday</h2>
+
+      <div className="row-flex">
+        {data.continents?.map((place) => (
+          <Card key={place.id} {...place} />
+        ))}
+      </div>
+    </div>
+  );
 }
